d8acb10c0d8e0a14eb5cfa4c28fa919d7921f48f
mhande_amalou <80551146+mhande97-dz@users.noreply.github.com>
2022-07-16 22:59:51 +0100

    initial commit

---
+++ src/app.js
@@ -0,0 +1,9 @@
+/******************************************
+ *  Author : Mhande Amalou   
+ *  Created On : Sat Jul 16 2022
+ *  File : app.js
+ *******************************************/
+
+import './sass/app.scss';
+
+console.log('its works');
