fe505670e39121a95ceffe04fa27255b031d6920
mhande_amalou <80551146+mhande97-dz@users.noreply.github.com>
2022-07-28 19:16:56 +0100

    initial commit

--- src/app.js
+++ src/app.js
@@ -6,3 +6,41 @@
 
 import "./sass/app.scss";
 
+
+//mobile menu functionality show & hide
+const primaryNav = document.querySelector(".primary-navigation"),
+    navToggleBtn = document.querySelector(".mobile-nav-toggle");
+
+
+navToggleBtn.addEventListener("click", () => {
+    const visiblity = primaryNav.getAttribute("data-expanded");
+
+
+    if (visiblity === "false") {
+        primaryNav.setAttribute("data-expanded", true);
+        navToggleBtn.setAttribute("aria-expanded",true);
+        
+    } else {
+        primaryNav.setAttribute("data-expanded", false);
+        navToggleBtn.setAttribute("aria-expanded",false);
+    }
+});
+
+//arrow up & down toggle
+const listItem = document.querySelectorAll(".has-dropdown");
+
+listItem.forEach(element => {
+    element.addEventListener("click", () => {
+    
+            const visiblity = element.getAttribute("aria-expanded");
+        
+            if (visiblity === "false") {
+                element.setAttribute("aria-expanded", true);
+                
+            } else {
+                element.setAttribute("aria-expanded", false);
+            }
+        });
+        
+        
+});
